f7c88b63d2f6930aef948e8a1a37aead4b490e34
Chimda Brignoli <chimdabrignoli@gmail.com>
2022-06-13 19:10:10 +0200

    issue 1: close modale done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClosed = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,12 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// issue #1: fermer la modale 
+// close modal event
+modalBtnClosed.forEach((btn)=> btn.addEventListener("click", closeModal));
+
+// close modal form
+function closeModal() {
+  modalbg.style.display="none";
+}
 
